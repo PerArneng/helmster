7a80e75062db8c726917ac03cc276ecfe369eec8
Per Arneng <per.arneng@scalebit.com>
2023-03-10 21:27:05 +0100

    improved the init command

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -3,10 +3,10 @@ use clap::{Arg, Command};
 pub fn create_init_command() -> Command {
     Command::new("init")
         .about("Inits a new Helmster project")
-        .arg(Arg::new("one")
-            .short('o')
-            .long("one")
+        .arg(Arg::new("name")
+            .short('n')
+            .long("name")
             .required(true)
-            .value_name("one")
+            .value_name("name")
         )
 }
--- src/main.rs
+++ src/main.rs
@@ -11,23 +11,23 @@ fn main() {
         .version(version)
         .author("Per Arneng <per.arneng@scalebit.com>")
         .about("A tool that generates Helm charts that can run a scripts without the need for a custom base images")
-        .arg(Arg::new("one")
-            .short('o')
-            .long("one")
-            .required(true)
-            .value_name("one"))
         .subcommands(vec![
             create_init_command(),
         ])
         .get_matches();
 
-    println!(
-        "two: {:?}",
-        matches.get_one::<String>("two").expect("required")
-    );
-    println!(
-        "one: {:?}",
-        matches.get_one::<String>("one").expect("required")
-    );
+    match matches.subcommand_matches("init") {
+        Some(matches) => {
+            println!("init");
+            //println!("name: {:?}", matches.value_of("name").expect("required"));
+            if let Some(c) = matches.get_one::<String>("name") {
+                println!("Value for -n: {}", c);
+            }
+        },
+        None => {
+            println!("no subcommand");
+        }
+    }
+
 
 }
